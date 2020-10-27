911a041c86573182ab84c1e9b08255689f691c4a
memirsajlow <marta.emirsajlow@gmail.com>
2020-10-27 10:52:53 +0100

    prepared repo for mountain app

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./Header.scss"
+
+const Header = () => {
+    return (
+      <header className="App-header">
+      <h1>Yoł łorking dżost fajn!</h1>
+     </header>
+    );
+  };export default Header;
