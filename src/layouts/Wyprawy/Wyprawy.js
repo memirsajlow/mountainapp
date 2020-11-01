1808018cec267c44bb47ca551c3805f2aac84951
memirsajlow <marta.emirsajlow@gmail.com>
2020-11-01 18:15:55 +0100

    added descriptions about summits

--- src/layouts/Wyprawy/Wyprawy.js
+++ src/layouts/Wyprawy/Wyprawy.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import "./Wyprawy.scss";
-import DSC03709 from "../../images/DSC03709.JPG";
+import DSC02586 from "../../images/DSC02586.JPG";
+import SAM_2398 from "../../images/SAM_2398.JPG";
+import SAM_1336 from "../../images/SAM_1336.JPG";
 
 const Wyprawy = () => {
   return (
@@ -11,22 +13,22 @@ const Wyprawy = () => {
         <div className="articles">
 
             <article>
-                <img src={DSC03709} alt="DSC03709" width="200px" height="200px"/>
-                <h3>LOREM IPSUM SIT DOLOR</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia quis at obcaecati recusandae tempora? In vitae libero itaque beatae veniam perferendis, et magnam. Suscipit illo quaerat deserunt veritatis in inventore.</p>
-                <a href="#" className="read-more">READ MORE</a>
+                <img src={DSC02586} alt="DSC025869" width="200px" height="200px"/>
+                <h3>MONT BLANC (4808,72 m n.p.m.). Alpy Zachodnie</h3>
+                <p>Najwyższy szczyt Alp położony w Masywie Mont Blanc, w Alpach Graickich, w Alpach Zachodnich. Potocznie nazywanym często Dachem Europy, choć to, czy jest najwyższym szczytem tego Kontynentu jest przedmiotem sporów, wynikających z różnych definicji granic Europy.</p>
+                <a href="#" className="read-more">Więcej</a>
             </article>
             <article>
-                <img src={DSC03709} alt="DSC03709" width="200px" height="200px"/>
-                <h3>LOREM IPSUM SIT DOLOR</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia quis at obcaecati recusandae tempora? In vitae libero itaque beatae veniam perferendis, et magnam. Suscipit illo quaerat deserunt veritatis in inventore.</p>
-                <a href="#" className="read-more">READ MORE</a>
+                <img src={SAM_2398} alt="SAM_2398" width="200px" height="200px"/>
+                <h3>PIK LENINA (7134 m n.p.m.). Góry Zaałajskie w Pamirze.</h3>
+                <p>Szczyt w Górach Zaałajskich w Pamirze, w Azji, na granicy Tadżykistanu i Kirgistanu, drugi co do wysokości (po Szczycie Ismaila Samaniego) w górach Pamiru. Do czasu rozpadu Związku Radzieckiego był drugim co do wysokości szczytem radzieckiego Pamiru i trzecim w ogóle szczytem ZSRR. </p>
+                <a href="#" className="read-more">Więcej</a>
             </article>
             <article>
-                <img src={DSC03709} alt="DSC03709" width="200px" height="200px"/>
-                <h3>LOREM IPSUM SIT DOLOR</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia quis at obcaecati recusandae tempora? In vitae libero itaque beatae veniam perferendis, et magnam. Suscipit illo quaerat deserunt veritatis in inventore.</p>
-                <a href="#" className="read-more">READ MORE</a>
+                <img src={SAM_1336} alt="SAM_1336" width="200px" height="200px"/>
+                <h3>CASTOR (4228 m n.p.m.) i POLLUX (4092 m n.p.m.). Alpy Pennińskie. </h3>
+                <p>Castor i Pollux nazwane są bliźniakami (niem. „Zwillinge”). Szczyt można zdobyć ze schronisk Monte Rosa Hut (2795 m) i Gandegg Hütte (3029 m) po stronie szwajcarskiej oraz Rifugio Quintino Sella (3585 m) i Rifugio Ottorino Mezzalama (3036 m) po stronie włoskiej. Lodowiec pod szczytem to Lodowiec Zwilling. </p>
+                <a href="#" className="read-more">Więcej</a>
             </article>
         </div>
         </div>
