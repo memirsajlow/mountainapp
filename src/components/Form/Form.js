bcc0b3b939e396d6d6614bb3fe995cdffd8629ea
memirsajlow <marta.emirsajlow@gmail.com>
2020-11-25 16:43:42 +0100

    added sending emails

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,53 +1,114 @@
-import React from 'react';
+
+
 
 import "./Form.scss";
 
-class Form extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: '',
-        email: '',
-        message: ''
-      }
-    }
-  
-    render() {
-      return(
-        <div className="Form">
-          <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-            <div className="form-group">
-              <label className="name">Twoje imię</label>
-              <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-            </div>
-            <div className="form-group">
-              <label className="exampleInputEmail1">Adres e-mail</label>
-              <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-            </div>
-            <div className="form-group">
-              <label className="message">Wiadomość</label>
-              <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-            </div>
-            <button type="submit" className="btn btn-primary">Wyślij</button>
-          </form>
-        </div>
-      );
-    }
-  
-    onNameChange(event) {
-      this.setState({name: event.target.value})
+import emailjs from "emailjs-com";
+import React from 'react';
+
+export default function Form() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+    emailjs.sendForm('service_319q06k', 'template_1gkh5m6', e.target, 'user_1RqaaZ5iX8CJzcjhlsI6m')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
     }
 
-    onEmailChange(event) {
-        this.setState({email: event.target.value})
-      }
+    return(
+        <div>
+            <div className="container">
+            <form onSubmit={sendEmail}>
+                    <div className="row pt-5 mx-auto">
+                        <div className="col-8 form-group mx-auto">
+                            <input type="text" className="form-control" placeholder="Name" name="name"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                        </div>
+                        <div className="col-8 pt-3 mx-auto">
+                            <input type="submit" className="btn btn-info" value="Send Message"></input>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+
+// class Form extends React.Component {
+//     constructor(props) {
+//       super(props);
+//        this.state = { feedback: '', name: '', email: '', message: '' };
+// 	    this.onNameChange = this.onNameChange.bind(this);
+//       this.handleSubmit = this.handleSubmit.bind(this);
+//       this.onEmailChange = this.onEmailChange.bind(this);
+//       this.onMessageChange = this.onMessageChange.bind(this);
+//       }
     
-      onMessageChange(event) {
-        this.setState({message: event.target.value})
-      }
+  
+//     render() {
+//       return(
+//         <div className="Form">
+//           <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+//             <div className="form-group">
+//               <label className="name">Twoje imię</label>
+//               <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+//             </div>
+//             <div className="form-group">
+//               <label className="email@example.com">Adres e-mail</label>
+//               <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+//             </div>
+//             <div className="form-group">
+//               <label className="message">Wiadomość</label>
+//               <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+//             </div>
+//             <button type="submit" className="btn btn-primary">Wyślij</button>
+//           </form>
+//         </div>
+//       );
+//     }
+  
+//     onNameChange(event) {
+//       this.setState({name: event.target.value})
+//     }
+
+//     onEmailChange(event) {
+//         this.setState({email: event.target.value})
+//       }
+      
+//       onMessageChange(event) {
+//         this.setState({message: event.target.value})
+//       }
     
-      handleSubmit(event) {
-      }
-    }
+//       handleSubmit (event) {
+//         const templateId = 'template_5m78nqs';
+      
+//         this.sendFeedback(templateId, {message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email})
+//         }
+      
+//         sendFeedback (templateId, name, email, message) {
+//         window.emailjs.send(
+//           'gmail', templateId,
+//           name, email, message
+//           ).then(res => {
+//             console.log('Email successfully sent!')
+//           })
+//           // Handle errors here however you like, or use a React error boundary
+//           .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+//         }
+//     }
     
-    export default Form;
+//    export default Form;
